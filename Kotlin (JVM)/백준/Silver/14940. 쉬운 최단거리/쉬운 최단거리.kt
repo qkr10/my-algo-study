3b0989f5f5f4351428bca7d8a3760494d43eef04
qkr10 <qkrtngks10@naver.com>
2024-12-05 21:47:59 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 1076 ms, Memory: 82240 KB -BaekjoonHub

--- Kotlin (JVM)/백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.kt
+++ Kotlin (JVM)/백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.kt
@@ -1,4 +1,35 @@
-import java.util.*
+class PriorityQueue<T>(e : T, c : (T, T) -> Boolean) {
+    var tree : ArrayList<T> = arrayListOf(e)
+    var compare : (T, T) -> Boolean = c
+    fun add(v : T) {
+        tree.add(v)
+        var index = tree.size - 1
+        while (index > 1) {
+            if (compare(tree[index], tree[index/2])) {
+                tree[index] = tree[index/2].also { tree[index/2] = tree[index] }
+            }
+            index /= 2
+        }
+    }
+    fun poll() : T {
+        val result = tree[1]
+        tree[1] = tree.last()
+        tree.removeLast()
+        var index = 1
+        while (index < tree.size) {
+            val l = index * 2
+            val r = l + 1
+            var swapIndex = index
+            if (l < tree.size && compare(tree[l], tree[swapIndex])) swapIndex = l
+            if (r < tree.size && compare(tree[r], tree[swapIndex])) swapIndex = r
+            if (swapIndex == index) break
+            tree[swapIndex] = tree[index].also { tree[index] = tree[swapIndex] }
+            index = swapIndex
+        }
+        return result
+    }
+    fun isNotEmpty() : Boolean { return tree.size != 1 }
+}
 
 fun dijkstra(input : Array<IntArray>, start : IntArray, size : IntArray) : Array<IntArray> {
     val delta = arrayOf(
@@ -12,7 +43,7 @@ fun dijkstra(input : Array<IntArray>, start : IntArray, size : IntArray) : Array
     val visit = Array(n) {IntArray(m)}
     val output = Array(n) {IntArray(m)}
 
-    val pq = PriorityQueue<IntArray>(compareBy { it[0] })
+    val pq = PriorityQueue(IntArray(0)) {a, b -> a[0] < b[0]}
     pq.add(intArrayOf(0, start[0], start[1]))
     visit[start[0]][start[1]] = 1
     while (pq.isNotEmpty()) {
@@ -30,13 +61,12 @@ fun dijkstra(input : Array<IntArray>, start : IntArray, size : IntArray) : Array
         }
     }
 
-    for (y in 0 until n) {
+    for (y in 0 until n)
         for (x in 0 until m) {
             if (visit[y][x] == 1) continue
             if (input[y][x] != 1) continue
             output[y][x] = -1
         }
-    }
     return output
 }
 
