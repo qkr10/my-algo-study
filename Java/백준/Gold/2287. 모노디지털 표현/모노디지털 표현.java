fa9f5c943f0b7d3816f59fb8cd783f4c2d7784c1
qkr10 <qkrtngks10@naver.com>
2025-01-03 02:14:16 +0900

    [Gold I] Title: 모노디지털 표현, Time: 244 ms, Memory: 18936 KB -BaekjoonHub

--- Java/백준/Gold/2287. 모노디지털 표현/모노디지털 표현.java
+++ Java/백준/Gold/2287. 모노디지털 표현/모노디지털 표현.java
@@ -26,15 +26,17 @@ public class Main {
     }
     static int solve() {
         for (int i = 1; i < 9; i++) {
-            dp.get(i).add(arr[i-1]);
-            for (int j = 1; j < i; j++) {
-                for (int a : dp.get(j)) {
-                    for (int b : dp.get(i - j)) {
-                        dp.get(i).add(a + b);
-                        dp.get(i).add(a - b);
-                        dp.get(i).add(a * b);
-                        if (b != 0)
-                            dp.get(i).add(a / b);
+            if (dp.get(i).isEmpty()) {
+                dp.get(i).add(arr[i-1]);
+                for (int j = 1; j < i; j++) {
+                    for (int a : dp.get(j)) {
+                        for (int b : dp.get(i - j)) {
+                            dp.get(i).add(a + b);
+                            dp.get(i).add(a - b);
+                            dp.get(i).add(a * b);
+                            if (b != 0)
+                                dp.get(i).add(a / b);
+                        }
                     }
                 }
             }
