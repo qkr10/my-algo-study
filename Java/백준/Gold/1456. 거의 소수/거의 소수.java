c1e8f9774eb49ad6f080d72296143f16300e3d59
qkr10 <qkrtngks10@naver.com>
2025-01-06 04:20:40 +0900

    [Gold V] Title: 거의 소수, Time: 340 ms, Memory: 53400 KB -BaekjoonHub

---
+++ Java/백준/Gold/1456. 거의 소수/거의 소수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        long[] nm = Arrays.stream(line).mapToLong(Long::parseLong).toArray();
+        int root = (int) Math.sqrt(nm[1]) + 1;
+        int[] eratos = new int[root+1];
+        for (int i = 2; i <= root; i++)
+            if (eratos[i] == 0)
+                for (int j = i*2; j <= root; j += i)
+                    eratos[j] = 1;
+        
+        int result = 0;
+        for (int i = 2; i <= root; i++)
+            if (eratos[i] == 0)
+                for (double j = (long)i * i; j <= nm[1]; j *= i)
+                    if (nm[0] <= j)
+                        result++;
+        System.out.println(result);
+    }
+}
