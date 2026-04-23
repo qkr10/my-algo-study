a05a10a7481f112e9d3b1434978efecaace77491
qkr10 <qkrtngks10@naver.com>
2026-04-23 22:17:55 +0900

    [level 4] Title: 4단 고음, Time: 12.92 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ C++/프로그래머스/4/1831. 4단 고음/4단 고음.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using mvii = map<vi, int>;
+
+mvii visit;
+
+int reverse_count(int n, int remain_star, int count_plus) {
+    if (n == 1) {
+        if (count_plus > 0)
+            return 0;
+        return 1;
+    }
+    
+    vi key{n, count_plus};
+    if (visit.find(key) != visit.end())
+        return visit[key];
+    
+    int ret = 0;
+    if (remain_star > 0 && count_plus >= 2 && n % 3 == 0)
+    	ret += reverse_count(n / 3, remain_star - 1, count_plus - 2);
+    if (remain_star * 2 > count_plus)
+	    ret += reverse_count(n - 1, remain_star, count_plus + 1);
+    return visit[key] = ret;
+}
+
+int solution(int n) {
+    visit = mvii();
+    
+    int star = 1;
+    for (; star < 30; star++) {
+        ll min_count = pow(3, star) + 2 * star;
+        ll max_count = 2 * pow(3, star) - 1;
+        if (n <= max_count) {
+            if (n < min_count)
+                return 0;
+            break;
+        }
+    }
+    
+    return reverse_count(n, star, 0);
+}
